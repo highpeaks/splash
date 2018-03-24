a28eebd85fdc1eab96d05ea7d0128c39f248532b
highpeaks <darek.bittner@gmail.com>
2018-03-24 13:58:45 -0400

    reorder files

--- server.js
+++
@@ -1,8 +0,0 @@
-//server
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-app.use(express.static(__dirname + '/public'));
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
